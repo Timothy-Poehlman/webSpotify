79402ac96e2816fc0da2d7b64b6ed16177512ba6
Tim P <timothypoehlman@gmail.com>
2020-09-18 20:59:06 -0700

    moved stuff over, going to edit to try and remove errors

--- webspotify/src/App.js
+++ webspotify/src/App.js
@@ -4,14 +4,16 @@
 
 import React from 'react';
 import Player from './Player.js';
-import PlaylistLinkForm from './PlaylistConverter.js';
 import hash from "./hash";
 import logo from './logo192.png';
+import { useForm, useField, splitFormProps } from 'react-form';
+import axios from 'axios';
 import * as $ from 'jquery';
 import './index.css';
 export const authEndpoint = 'https://accounts.spotify.com/authorize?';
 
 const clientId = process.env.REACT_APP_SPOTIFYCLIENTID;
+const youtubeKey = process.env.REACT_APP_YOUTUBEKEY;
 const redirectUri = "http://localhost:3000";
 const scopes = [
     "user-read-currently-playing",
@@ -19,6 +21,49 @@ const scopes = [
     "playlist-modify",
     "playlist-modify-private",
 ];
+
+const {
+    Form,
+    meta: { isSubmitting, canSubmit }
+} = useForm({
+    onSubmit: async (values, instance) => {
+        //send values to getPlaylist, once names are retrieved send info to giveToSpotify to convert
+        console.log(values);
+        var playlist = await this.getPlaylist(values.playlistLink);
+        console.log("Got Playlist!");
+        //find corresponding id
+        var index;
+        for(var i=0;i<this.state.userPlaylists.length;i++)
+        {
+            if(values.playlistName == this.state.userPlaylists[i]){index=i;break;}
+        }
+        this.giveToSpotify(playlist, values.playlistReName, this.state.playlistIds[index], this.state.user_id, this.state.token)
+    }
+})
+// from React Form- Basic Form example by tannerlinsley
+const InputField = React.forwardRef((props, ref) => {
+    // Let's use splitFormProps to get form-specific props
+    const [field, fieldOptions, rest] = splitFormProps(props);
+
+    // Use the useField hook with a field and field options
+    // to access field state
+    const {
+        meta: { error, isTouched, isValidating },
+        getInputProps
+    } = useField(field, fieldOptions);
+
+    // Build the field
+    return (
+        <>
+            <input {...getInputProps({ ref, ...rest })} />{" "}
+            {isValidating ? (
+                <em>Validating...</em>
+            ) : isTouched && error ? (
+                <em>{error}</em>
+            ) : null}
+        </>
+    );
+});
 
 //sets the clock
 export class Clock extends React.Component {
@@ -72,6 +117,14 @@ export default class App extends React.Component {
             this.getUserPlaylists(this.state.user_id,_token)
         }
         this.interval = setInterval(() => this.tick(), 5000);
+    }
+
+    /*
+        validates that a url is from youtube
+    */
+    validate(url) {
+        var regExp = /^(?:https?:\/\/)?(?:www\.)?youtube\.com(?:\S+)?$/;
+        return url.match(regExp) && url.match(regExp).length > 0;
     }
 
     getUserInfo(token) {
@@ -163,6 +216,136 @@ export default class App extends React.Component {
         });
     }
 
+    /*
+        Description: adds the playlist to the given playlist name, if it does not exist create a new one
+        parameters: playlist: an array of video info
+                    playlistname: name of playlist that this will be added to in spotify
+    */
+    async giveToSpotify(youtubePlaylist, playlistName, playlistId, userId, token) {
+        var playlist = [];
+    
+        //convert youtube list to best fitting spotify playlist song names - for now it just searches the title, but later we can try to extract the name of the artist
+        for (var i = 0; i < youtubePlaylist.length; i++) {
+            playlist.push(
+                await $.ajax({
+                    url: "https://api.spotify.com/v1/search",
+                    type: "GET",
+                    data: JSON.stringify({q:youtubePlaylist[i].snippet.title, type:"track"}),
+                    beforeSend: (xhr) => {
+                        xhr.setRequestHeader("Authorization", "Bearer " + token);
+                    },
+                    error: function (response) {
+                        alert(response.statusText);
+                        console.log(response.statusText);
+                        console.log("failed");
+                    },
+                    success: (data) => {
+                        if(data.length == 0)
+                        {
+                            console.log(data.tracks.items);
+                            return data.tracks.items[0].uri;
+                        }
+                        else{console.log("Could not find song");}
+                    }
+                })
+            );
+        }
+        console.log(playlist);
+    
+    
+    
+    
+        //create new playlist if requested
+        var playlist_id = playlistId;
+        if(playlistId == "newPlaylist")
+        {
+            playlist_id = await this.createSpotifyPlaylist(userId, playlistName, token);
+            playlist_id = playlist_id.id;
+        }
+        console.log(playlist_id);
+        //adds songs to the playlist
+        var songUris;
+        //construct uri, should be in format: uri1,uri2,uri3,...
+        for (var i = 0; i < playlist.length; i++) {
+            songUris = songUris + ',' + playlist.pop();
+        }
+    
+        await $.ajax({
+            url: "https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks",
+            type: "POST",
+            data: JSON.stringify({uris:songUris}),
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            error: function (response) {
+                alert(response.statusText);
+                console.log(response.statusText);
+                console.log("failed");
+            },
+            success: (data) => {
+                if(data)
+                {
+                    console.log(data.tracks.items);
+                    return data.tracks.items[0].uri;
+                }
+                else{console.log("Could not find song");}
+            }
+        })
+    }
+
+    /*
+        Description: Retrieves a list of names of every video in the playlist
+    */
+    async getPlaylist(link) {
+        console.log("Getting Playlist from Youtube");
+        var reg = new RegExp("[&?]list=([a-z0-9_-]+)", "i");
+        var match = reg.exec(link);
+        var id;
+        var data = [];
+
+        if (match && match[1].length > 0 && this.validate(link)) {
+            id = match[1];
+        } else {
+            return "could not find id";
+        }
+
+        //needs to be await for the promise to be fulfilled
+        data = await axios.get('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=' + id + '&maxResults=50&part=snippet&key=' + youtubeKey)
+            .then((response) => {
+                console.log(response.data.items);
+                return response.data.items;
+            }, (error) => {
+                console.log(error);
+            });
+
+        console.log("title: " + data[0].snippet.title);
+
+        return data;
+
+    }
+
+    async createSpotifyPlaylist(userId, name, token)
+    {
+        return await $.ajax({
+            url: "https://api.spotify.com/v1/users/" + userId + '/playlists',
+            type: "POST",
+            data: JSON.stringify({name: name,}),
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            error: function (response) {
+                alert(response.statusText);
+                console.log(response.statusText);
+                console.log("failed");
+            },
+            success: (data) => {
+                console.log(data.id);
+                return data.id;
+            }
+        });
+    }
+
+
     tick() {
         if (this.state.token) {
             this.getCurrentlyPlaying(this.state.token);
@@ -173,6 +356,38 @@ export default class App extends React.Component {
         }
     }
 
+    MultiSelectField(props) {
+        const [field, fieldOptions, { options, ...rest }] = splitFormProps(props);
+    
+        const {
+            value = [],
+            setValue,
+            meta: { isTouched, error }
+        } = useField(field, fieldOptions);
+    
+        const handleSelectChange = e => {
+            const selected = Array.from(e.target.options)
+                .filter(option => option.selected)
+                .map(option => option.value);
+    
+            setValue(selected);
+        };
+    
+        return (
+            <>
+                <select {...rest} value={value} onChange={handleSelectChange} multiple>
+                    <option disabled value="" />
+                    {options.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+                {isTouched && error ? <em>{error}</em> : null}
+            </>
+        );
+    }
+    
     render() {
         return (
             <div className="App">
@@ -195,12 +410,37 @@ export default class App extends React.Component {
 
                     {/* Youtube -> Spotify playlist conversion */}
                     {this.state.token && (
-                        <PlaylistLinkForm
-                            user_id={this.state.user_id}
-                            userPlaylists={this.state.userPlaylists}
-                            playlistIds={this.state.userPlaylistsIds}
-                            token={this.state.token}
-                        />
+                        <div className="App">
+                            <div className="main-wrapper">
+                                <Form>
+                                    <div>
+                                        <label>
+                                            Youtube Playlist Link: <InputField field="playlistLink" validate={value => (!value ? "Required" : false)} />
+                                        </label>
+                                    </div>
+                                    <div id="playlistNameInput">
+                                        <label>
+                                            Spotify Playlist Name (Only Required if creating a new Playlist): <InputField field="playlistReName" validate={value => (!value ? "Required" : false)} />
+                                        </label>
+                                        <label>
+                                            Choose Playlist:{" "}
+                                            <this.MultiSelectField
+                                                field="playlistName"
+                                                options={
+                                                    this.state.userPlaylists
+                                                }
+                                                validate={value => (!value ? "Required" : false)}
+                                            />
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <button type="submit" disabled={!canSubmit}>
+                                            Submit
+                                        </button>
+                                    </div>
+                                </Form>
+                            </div>
+                        </div>
                     )}
                 </header>
             </div>
