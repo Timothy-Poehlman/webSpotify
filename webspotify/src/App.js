02aeb449c9314df503a10b42748a59127879e018
Tim P <timothypoehlman@gmail.com>
2020-09-18 20:45:56 -0700

    security issues stopping progress with playlistconverter, going to try moving it all to app.js since the ones there seem to work

--- webspotify/src/App.js
+++ webspotify/src/App.js
@@ -9,7 +9,6 @@ import hash from "./hash";
 import logo from './logo192.png';
 import * as $ from 'jquery';
 import './index.css';
-
 export const authEndpoint = 'https://accounts.spotify.com/authorize?';
 
 const clientId = process.env.REACT_APP_SPOTIFYCLIENTID;
@@ -17,6 +16,8 @@ const redirectUri = "http://localhost:3000";
 const scopes = [
     "user-read-currently-playing",
     "user-read-playback-state",
+    "playlist-modify",
+    "playlist-modify-private",
 ];
 
 //sets the clock
@@ -46,9 +47,15 @@ export default class App extends React.Component {
             },
             is_playing: "Paused",
             progress_ms: 0,
-            device: "None"
+            device: "None",
+            no_data: false,
+            user_id: null,
+            userPlaylists: [],
+            userPlaylistsIds: [],
         };
         this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+        this.getUserInfo = this.getUserInfo.bind(this);
+        this.getUserPlaylists = this.getUserPlaylists.bind(this);
         this.tick = this.tick.bind(this);
     }
 
@@ -61,8 +68,66 @@ export default class App extends React.Component {
                 token: _token
             });
             this.getCurrentlyPlaying(_token);
+            this.getUserInfo(_token);
+            this.getUserPlaylists(this.state.user_id,_token)
         }
-        this.interval = setInterval(() => this.tick(), 3000);
+        this.interval = setInterval(() => this.tick(), 5000);
+    }
+
+    getUserInfo(token) {
+        $.ajax({
+            url: "https://api.spotify.com/v1/me",
+            type: "GET",
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            error: function (response) {
+                alert(response.statusText);
+                console.log(response.statusText);
+                console.log("failed");
+            },
+            success: (data) => {
+                if(!data) {
+                    console.log("No data found!");
+                }
+                else{
+                    this.setState({
+                        user_id: data.id,
+                    });
+                }
+            }
+        });
+    }
+
+    //gets a list of playlist data of all of the users playlists (Spotify)
+    getUserPlaylists(userId, token) {
+        $.ajax({
+            url: "https://api.spotify.com/v1/users/" + userId + '/playlists',
+            type: "GET",
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            error: function (response) {
+                alert(response.statusText);
+                console.log(response.statusText);
+                console.log("failed");
+            },
+            success: (data) => {
+                var playlist = [];
+                var playlistIds = [];
+                for(var i=0;i<data.items.length;i++)
+                {
+                    playlistIds.push(data.items[i].id);
+                    playlist.push(data.items[i].name);
+                }
+                playlist.push("Create New Playlist");
+                playlistIds.push("newPlaylist");
+                this.setState({
+                    userPlaylists: playlist,
+                    userPlaylistsIds: playlistIds,
+                });
+            }
+        });
     }
 
     //use this format for all of the api info
@@ -80,21 +145,31 @@ export default class App extends React.Component {
                 console.log("failed");
             },
             success: (data) => {
-                this.setState({
-                    item: data.item,
-                    is_playing: data.is_playing,
-                    progress_ms: data.progress_ms,
-                    device: data.device.name,
-                });
-                console.log("Got Data");
+                if(!data) {
+                    this.setState({
+                        no_data: true,
+                    });
+                }
+                else{
+                    this.setState({
+                        item: data.item,
+                        is_playing: data.is_playing,
+                        progress_ms: data.progress_ms,
+                        device: data.device.name,
+                        no_data: false,
+                    });
+                }
             }
         });
     }
 
     tick() {
-        console.log("tick");
         if (this.state.token) {
             this.getCurrentlyPlaying(this.state.token);
+            this.getUserInfo(this.state.token);
+            this.getUserPlaylists(this.state.user_id,this.state.token);
+            console.log(this.state.playlistIds);
+            console.log("Got Data");
         }
     }
 
@@ -120,7 +195,12 @@ export default class App extends React.Component {
 
                     {/* Youtube -> Spotify playlist conversion */}
                     {this.state.token && (
-                        <PlaylistLinkForm/>
+                        <PlaylistLinkForm
+                            user_id={this.state.user_id}
+                            userPlaylists={this.state.userPlaylists}
+                            playlistIds={this.state.userPlaylistsIds}
+                            token={this.state.token}
+                        />
                     )}
                 </header>
             </div>
--- webspotify/src/PlaylistConverter.js
+++ webspotify/src/PlaylistConverter.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { useForm, useField, splitFormProps } from 'react-form';
 import axios from 'axios';
+import * as $ from 'jquery';
 
 
 const youtubeKey = process.env.REACT_APP_YOUTUBEKEY;
@@ -37,51 +38,173 @@ const InputField = React.forwardRef((props, ref) => {
 */
 function validate(url) {
     var regExp = /^(?:https?:\/\/)?(?:www\.)?youtube\.com(?:\S+)?$/;
-    return url.match(regExp)&&url.match(regExp).length>0;
+    return url.match(regExp) && url.match(regExp).length > 0;
 }
 
 /*
     Description: Retrieves a list of names of every video in the playlist
 */
-function getPlaylist(link)
-{
+async function getPlaylist(link) {
     console.log("Getting Playlist from Youtube");
-    var reg = new RegExp("[&?]list=([a-z0-9_-]+)","i");
+    var reg = new RegExp("[&?]list=([a-z0-9_-]+)", "i");
     var match = reg.exec(link);
     var id;
     var data = [];
 
-    if (match&&match[1].length>0&&validate(link)){
+    if (match && match[1].length > 0 && validate(link)) {
         id = match[1];
-    }else{
+    } else {
         return "could not find id";
     }
 
-    axios.get('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=' + id + '&part=snippet&key='+youtubeKey)
-        .then(res => {
-            data = res.data.items[0];
-        })
+    //needs to be await for the promise to be fulfilled
+    data = await axios.get('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=' + id + '&maxResults=50&part=snippet&key=' + youtubeKey)
+        .then((response) => {
+            console.log(response.data.items);
+            return response.data.items;
+        }, (error) => {
+            console.log(error);
+        });
 
-    console.log(data.snippit.title);
+    console.log("title: " + data[0].snippet.title);
+
+    return data;
+
 }
 
-/*
-    Description: asks user if they want to create a new one or add it to an existing one
-*/
-function checkType(playlist)
+async function createSpotifyPlaylist(userId, name, token)
 {
-
+    return await $.ajax({
+        url: "https://api.spotify.com/v1/users/" + userId + '/playlists',
+        type: "POST",
+        data: JSON.stringify({name: name,}),
+        beforeSend: (xhr) => {
+            xhr.setRequestHeader("Authorization", "Bearer " + token);
+        },
+        error: function (response) {
+            alert(response.statusText);
+            console.log(response.statusText);
+            console.log("failed");
+        },
+        success: (data) => {
+            console.log(data.id);
+            return data.id;
+        }
+    });
 }
 
 /*
     Description: adds the playlist to the given playlist name, if it does not exist create a new one
+    parameters: playlist: an array of video info
+                playlistname: name of playlist that this will be added to in spotify
+                exists: whether the playlist exists or if we will create a new one
 */
-function giveToSpotify(playlist, playlistName)
-{
+async function giveToSpotify(youtubePlaylist, playlistName, playlistId, userId, token) {
+    var playlist = [];
 
+    //convert youtube list to best fitting spotify playlist song names - for now it just searches the title, but later we can try to extract the name of the artist
+    for (var i = 0; i < youtubePlaylist.length; i++) {
+        playlist.push(
+            await $.ajax({
+                url: "https://api.spotify.com/v1/search",
+                type: "GET",
+                data: JSON.stringify({q:youtubePlaylist[i].snippet.title, type:"track"}),
+                beforeSend: (xhr) => {
+                    xhr.setRequestHeader("Authorization", "Bearer " + token);
+                },
+                error: function (response) {
+                    alert(response.statusText);
+                    console.log(response.statusText);
+                    console.log("failed");
+                },
+                success: (data) => {
+                    if(data.length == 0)
+                    {
+                        console.log(data.tracks.items);
+                        return data.tracks.items[0].uri;
+                    }
+                    else{console.log("Could not find song");}
+                }
+            })
+        );
+    }
+    console.log(playlist);
+
+
+
+
+    //create new playlist if requested
+    var playlist_id = playlistId;
+    if(playlistId == "newPlaylist")
+    {
+        playlist_id = await createSpotifyPlaylist(userId, playlistName, token);
+        playlist_id = playlist_id.id;
+    }
+    console.log(playlist_id);
+    //adds songs to the playlist
+    var songUris;
+    //construct uri, should be in format: uri1,uri2,uri3,...
+    for (var i = 0; i < playlist.length; i++) {
+        songUris = songUris + ',' + playlist.pop();
+    }
+
+    await $.ajax({
+        url: "https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks",
+        type: "POST",
+        data: JSON.stringify({uris:songUris}),
+        beforeSend: (xhr) => {
+            xhr.setRequestHeader("Authorization", "Bearer " + token);
+        },
+        error: function (response) {
+            alert(response.statusText);
+            console.log(response.statusText);
+            console.log("failed");
+        },
+        success: (data) => {
+            if(data)
+            {
+                console.log(data.tracks.items);
+                return data.tracks.items[0].uri;
+            }
+            else{console.log("Could not find song");}
+        }
+    })
 }
 
-export default function PlaylistLinkForm() {
+function MultiSelectField(props) {
+    const [field, fieldOptions, { options, ...rest }] = splitFormProps(props);
+
+    const {
+        value = [],
+        setValue,
+        meta: { isTouched, error }
+    } = useField(field, fieldOptions);
+
+    const handleSelectChange = e => {
+        const selected = Array.from(e.target.options)
+            .filter(option => option.selected)
+            .map(option => option.value);
+
+        setValue(selected);
+    };
+
+    return (
+        <>
+            <select {...rest} value={value} onChange={handleSelectChange} multiple>
+                <option disabled value="" />
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            {isTouched && error ? <em>{error}</em> : null}
+        </>
+    );
+}
+
+function PlaylistLinkForm(props) {
+    console.log(props.playlistIds);
     const {
         Form,
         meta: { isSubmitting, canSubmit }
@@ -89,8 +212,15 @@ export default function PlaylistLinkForm() {
         onSubmit: async (values, instance) => {
             //send values to getPlaylist, once names are retrieved send info to giveToSpotify to convert
             console.log(values);
-            await checkType(getPlaylist(values.playlistLink));
-            console.log("Converted!");
+            var playlist = await getPlaylist(values.playlistLink);
+            console.log("Got Playlist!");
+            //find corresponding id
+            var index;
+            for(var i=0;i<props.userPlaylists.length;i++)
+            {
+                if(values.playlistName == props.userPlaylists[i]){index=i;break;}
+            }
+            giveToSpotify(playlist, values.playlistReName, props.playlistIds[index], props.user_id, props.token)
         }
     })
     return (
@@ -102,8 +232,30 @@ export default function PlaylistLinkForm() {
                             Youtube Playlist Link: <InputField field="playlistLink" validate={value => (!value ? "Required" : false)} />
                         </label>
                     </div>
+                    <div id="playlistNameInput">
+                        <label>
+                            Spotify Playlist Name (Only Required if creating a new Playlist): <InputField field="playlistReName" validate={value => (!value ? "Required" : false)} />
+                        </label>
+                        <label>
+                            Choose Playlist:{" "}
+                            <MultiSelectField
+                                field="playlistName"
+                                options={
+                                    props.userPlaylists
+                                }
+                                validate={value => (!value ? "Required" : false)}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <button type="submit" disabled={!canSubmit}>
+                            Submit
+                        </button>
+                    </div>
                 </Form>
             </div>
         </div>
     );
 }
+
+export default PlaylistLinkForm;
